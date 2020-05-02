a255ebd862e692be37e69745528d7664af5c1360
iamernie8199 <iamernie8199@gmail.com>
2020-05-03 03:19:42 +0800

    Update example_q1.py

--- example_q1.py
+++ example_q1.py
@@ -60,15 +60,22 @@ for f in FILES:
     chroma_vector = np.sum(cxx, axis=1)
     # finding the maximal value in the chroma vector and considering the note 
     # name corresponding to the maximal value as the tonic pitch
-    key_ind = pitches[int(np.argmax(chroma_vector))]
-    """
+    key_ind = int(np.argmax(chroma_vector))
+
     # finding the correlation coefficient between the summed chroma vectors and 
     # the mode templates
     # Hint: utils.rotate(ar,n) may help you find different key mode template
-    mode = ?
+    MODE = {"major": [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1],
+            "minor": [1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0]}
+    KEY = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+    LABEL = [k + " major" if i < 12 else k + " minor" for i, k in enumerate(utils.rotate(KEY, 3) * 2)]
+    LABEL_str = dict(zip(LABEL, list(range(len(LABEL)))))
+    mode = KEY[key_ind] + ' major' if pearsonr(chroma_vector, utils.rotate(MODE['major'], key_ind)) > pearsonr(
+        chroma_vector, utils.rotate(MODE['minor'], key_ind)) else KEY[key_ind] + ' minor'
+
     if DB == 'GTZAN':
-        pred[gen].append(?)
-        else:
+        pred[gen].append(mode)
+    else:
         pred.append('?')  # you may ignore this when starting with GTZAN dataset
 ##########
 
@@ -77,11 +84,11 @@ if DB == 'GTZAN':
     label_list, pred_list = list(), list()
     print("Genre    \taccuracy")
     for g in GENRE:
-        ##########
+        ##################################################
         # TODO: Calculate the accuracy for each genre
         # Hint: Use label[g] and pred[g]
-        acc = ?
-        ##########
+        ##################################################
+        acc = len([i for i, j in zip(label[g], pred[g]) if i == j]) / len(label[g])
         print("{:9s}\t{:8.2f}%".format(g, acc))
         label_list += label[g]
         pred_list += pred[g]
@@ -89,11 +96,10 @@ else:
     label_list = label
     pred_list = pred
 
-##########
+##################################################
 # TODO: Calculate the accuracy for all file.
 # Hint1: Use label_list and pred_list.
-acc_all = ?
-##########
+##################################################
+acc_all = len([i for i, j in zip(label_list, pred_list) if i == j]) / len(label_list)
 print("----------")
 print("Overall accuracy:\t{:.2f}%".format(acc_all))
-"""
