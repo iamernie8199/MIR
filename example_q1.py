ffab38f5c01019d5a6409f63339e78a64c0a7f31
iamernie8199 <iamernie8199@gmail.com>
2020-05-03 00:03:20 +0800

    Update example_q1.py

--- example_q1.py
+++ example_q1.py
@@ -19,6 +19,8 @@ import numpy as np
 # %%
 import utils  # self-defined utils.py file
 
+pitches = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+
 DB = 'GTZAN'
 if DB == 'GTZAN':  # dataset with genre label classify at parent directory
     FILES = glob(DB + '/wav/*/*.wav')
@@ -50,27 +52,28 @@ for f in FILES:
     sr, y = utils.read_wav(f)
     ##########
     # TODO: Follow task1 description to give each audio file a key prediction.
-    """
+
     # compute the chromagram of audio data `y`
-    cxx = ?
-    chromagram.append(cxx) # store into list for further use
+    cxx = chroma_stft(y=y, sr=sr)
+    chromagram.append(cxx)  # store into list for further use
     # summing up all the chroma features into chroma vector
-    chroma_vector = ?
+    chroma_vector = np.sum(cxx, axis=1)
     # finding the maximal value in the chroma vector and considering the note 
     # name corresponding to the maximal value as the tonic pitch
-    key_ind = ?
+    key_ind = pitches[int(np.argmax(chroma_vector))]
+    """
     # finding the correlation coefficient between the summed chroma vectors and 
     # the mode templates
     # Hint: utils.rotate(ar,n) may help you find different key mode template
     mode = ?
-    if DB=='GTZAN':
+    if DB == 'GTZAN':
         pred[gen].append(?)
-    else: 
-        pred.append('?') # you may ignore this when starting with GTZAN dataset
-    ##########
+        else:
+        pred.append('?')  # you may ignore this when starting with GTZAN dataset
+##########
 
 print("***** Q1 *****")
-if DB=='GTZAN':
+if DB == 'GTZAN':
     label_list, pred_list = list(), list()
     print("Genre    \taccuracy")
     for g in GENRE:
@@ -79,12 +82,12 @@ if DB=='GTZAN':
         # Hint: Use label[g] and pred[g]
         acc = ?
         ##########
-        print("{:9s}\t{:8.2f}%".format(g,acc))
+        print("{:9s}\t{:8.2f}%".format(g, acc))
         label_list += label[g]
-        pred_list  += pred[g]
+        pred_list += pred[g]
 else:
     label_list = label
-    pred_list  = pred
+    pred_list = pred
 
 ##########
 # TODO: Calculate the accuracy for all file.
