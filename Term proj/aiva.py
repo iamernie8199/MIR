bd015f43bb49eebefef164f2ee8cb38827d1142e
iamernie8199 <iamernie8199@gmail.com>
2020-06-22 19:41:56 +0800

    Update aiva.py

--- Term proj/aiva.py
+++ Term proj/aiva.py
@@ -48,7 +48,7 @@ for f in tqdm(FILES):
     p_ref = np.max(power)
     loudness = librosa.power_to_db(p_mean, ref=p_ref)
     # Volumns and energy
-    v = np.mean(librosa.feature.rms(y, frame_length=1024, hop_length=512, center=False), axis=0)
+    v = np.mean(librosa.feature.rms(y, frame_length=1024, hop_length=512, center=False))
     e = np.sum(frame**2, axis=0)
 
     df = df.append([{
